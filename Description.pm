777f001623d1b6e6b2b3ec8222776bd8084efe55
USKamicadze <uskamicadze@gmail.com>
2015-05-09 13:16:27 +1000

    add Description module

---
+++ Description.pm
@@ -0,0 +1,82 @@
+package CATS::Formal::Description;
+use strict;
+use warnings;
+
+use BaseObj;
+use Constants;
+use parent -norequire, 'CATS::Formal::BaseObj';
+
+sub is_description {1;}
+
+sub new {
+    my $class = shift;
+    my $arg = shift;
+    my $self = bless {}, $class;
+    $self->{name} = $arg->{name};
+    $self->{type} = $arg->{type};
+    $self->{parent} = $arg->{parent};
+    $self->{children} = [];
+    $self->{constraints} = [];
+    $self->{attributes} = {};
+    $self->{parent}->add_child($self) if $self->{parent};
+    
+    return $self;
+}
+
+sub stringify {
+    my $self = shift;
+    my $deep = 0;
+    if (@_) {
+        $deep = shift;
+    }
+    
+    my $a = $self->{name} ? 'name=' . $self->{name}.' ' : '';
+    foreach (keys %{$self->{attributes}}){
+        my $e = $self->{attributes}->{$_}->stringify;
+        $a .= "$_=( $e ) ";
+    }
+    my $t = CATS::Formal::Constants::RFD_TYPES->{$self->{type}};
+    my $res = ' ' x $deep . "<$t $a>\n";
+    foreach my $child (@{$self->{children}}){
+        $res .= $child->stringify($deep + 1);
+    }
+    return $res . ' ' x $deep . "<$t/>\n";
+}
+
+sub find {
+    my ($self, $name) = @_;
+    my $cur = $self;
+    while ($cur) {
+        return $cur if $cur->{name} && $cur->{name} eq $name;
+        my $child = $cur->find_child($name);
+        return $child if $child;
+        $cur = $cur->{parent};        
+    }
+    return $cur;    
+}
+
+sub add_child {
+    push $_[0]->{children}, $_[1];
+}
+
+sub find_child {
+    my ($self, $arg) = @_;
+    foreach (@{$self->{children}}) {
+        return $_ if $_->{name} && $arg eq $_->{name};
+    }
+    return undef;
+}
+
+sub find_child_by_type {
+    my ($self, $type) = @_;
+    foreach (@{$self->{children}}) {
+        return $_ if $type == $_->{type};
+    }
+    return undef;
+}
+
+sub add_constraint {
+    push $_[0]->{constraints}, $_[1];
+}
+
+1;
