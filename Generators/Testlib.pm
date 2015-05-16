da7e03ffd4b229f83ae702bf9cd86dfcc907902e
USKamicadze <uskamicadze@gmail.com>
2015-05-16 19:52:43 +1000

    check range when read float

--- Generators/Testlib.pm
+++ Generators/Testlib.pm
@@ -179,8 +179,21 @@ sub generate_float_obj {
     $obj->{name_for_expr} = $prefix . $obj->{name};
     $obj->{declaration} = "double $obj->{name};\n";
     $fd->{obj} = $obj;
-    $obj->{reader} = $spaces."$obj->{name_for_expr} = $stream_name.readDouble();\n" .
-        $self->generate_constraints($fd, $spaces);
+    my $range = $fd->{attributes}->{range};
+    if ($range) {
+        my $a = 0;
+        my $b = 0;
+        if ($range->is_array) {
+            $a = $self->generate_expr($range->[0]);
+            $b = $self->generate_expr($range->[1]);
+        } else {
+            $b = $self->generate_expr($range);
+        }
+        $obj->{reader} = $spaces . "$obj->{name_for_expr} = $stream_name.readDouble($a, $b, \"$obj->{name}\");\n"
+    } else {
+        $obj->{reader} = $spaces . "$obj->{name_for_expr} = $stream_name.readDouble();\n";
+    }
+    $obj->{reader} .= $self->generate_constraints($fd, $spaces);
         
     return $obj;
 }
