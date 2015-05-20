88a83237182c305c4b6be23b024f52c885f40940
USKamicadze <uskamicadze@gmail.com>
2015-05-20 19:55:01 +1000

    generate struct Output, use it to compare answer and output

--- Generators/TestlibChecker.pm
+++ Generators/TestlibChecker.pm
@@ -3,6 +3,7 @@ package CATS::Formal::Generators::TestlibChecker;
 use TestlibBase;
 use parent -norequire, 'CATS::Formal::Generators::TestlibBase';
 
+my $pe = '_constraint_result_';
 sub generate_description {
     my ($self, $fd) = @_;
     if ($fd->{type} == CATS::Formal::Constants::FD_TYPES->{ROOT}) {
@@ -13,12 +14,18 @@ sub generate_description {
             $self->{declarations} .= $obj->{declaration};
         }
         
+        $self->{mode} = 'OUTPUT';
         my $output = $fd->find_child_by_type(CATS::Formal::Constants::FD_TYPES->{OUTPUT});
+        my @output_compare = ();
         foreach my $child(@{$output->{children}}){
-            my $obj = $self->generate_obj($child, '', 1);
+            my $obj = $self->generate_obj($child, '_output.', 1);
             $self->{output_reader} .= $obj->{reader};
-            $self->{declarations} .= $obj->{declaration};
+            $self->{output_declarations} .= $obj->{declaration};
+            push @output_compare, "f.$obj->{name} == s.$obj->{name}";
         }
+        my $output_compare = join ' && ', @output_compare;
+        $self->{output_compare} = $output_compare;
+        $self->{mode} = undef;
         return;
     } else { die "not implemented" };
 }
@@ -26,40 +33,49 @@ sub generate_description {
 sub pattern {
     my $self = shift;
     my $stream_name = $self->{stream_name};
+    my $output_compare = $self->{output_compare};
     return <<"END"
 #include "testlib.h"
+#define _test_(C, R) if (!(C)) quitf(R, "Condition failed: \"" #C "\" at line:%d", __LINE__)
 
 using namespace std;
 
-$self->{type_declarations}
+$self->{type_declarations}struct Output;
+typedef Output Answer;
+
 $self->{declarations}
-$self->{type_definitions}
+$self->{type_definitions}struct Output {
+    $self->{output_declarations}
+};
+bool operator==(const Output& f, const Output& s){
+    return $output_compare;
+}
+
+Output _pa;
+Answer _ja;
+
 $self->{functions}
 
 void readInput(InStream& $stream_name){
 $self->{input_reader}
 }
 
-void readOutput(InStream& $stream_name){
+Output readOutput(InStream& $stream_name, TResult $pe){
+    Output _output;
 $self->{output_reader}
-}
-
-void readAns(InStream& $stream_name){
-$self->{ans_reader}
+    return _output;
 }
 
 void _checkSolution(){
-    /*write ckecker here*/
+    _test_(_pa == _ja, _wa);
 }
 
 int main(int argc, char** argv){
-    setName("compares two signed integers");
     registerTestlibCmd(argc, argv);
-    
+    /* comment out, if input not needed */
     readInput(inf);
-    readOutput(ouf);
-    readAns(ans);
-    
+    _pa = readOutput(ouf, _wa);
+    _ja = readOutput(ans, _fail);
     _checkSolution();
     
     quit(_ok, "all ok");
