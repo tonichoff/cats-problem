5822893fa759a9b968494c06596190cfc6c64cb1
USKamicadze <uskamicadze@gmail.com>
2015-05-29 18:14:25 +1000

    add delimiter string between Expression classes

--- Expressions.pm
+++ Expressions.pm
@@ -25,6 +25,7 @@ sub is_float{0;}
 sub is_int{0;}
 sub is_string{0;}
 
+##############################################################################
 package CATS::Formal::Expressions::Binary;
 # left - expr, right - expr, op - token
 use parent -norequire , 'CATS::Formal::Expressions::BaseExpression';
@@ -34,6 +35,7 @@ sub stringify {
     $_[0]->{left}->stringify . $_[0]->{op} . $_[0]->{right}->stringify;
 }
 
+##############################################################################
 package CATS::Formal::Expressions::Unary;
 #op - token, node - expr
 use parent -norequire , 'CATS::Formal::Expressions::BaseExpression';
@@ -42,6 +44,7 @@ sub stringify {
     $_[0]->{op} . $_[0]->{node};
 }
 
+##############################################################################
 package CATS::Formal::Expressions::Variable;
 #fd - fd
 use parent -norequire , 'CATS::Formal::Expressions::BaseExpression';
@@ -50,7 +53,7 @@ sub stringify {
     $_[0]->{fd}->{name};
 }
 
-
+##############################################################################
 package CATS::Formal::Expressions::Function;
 #name - string, params - [expr]
 use parent -norequire , 'CATS::Formal::Expressions::BaseExpression';
@@ -60,10 +63,12 @@ sub stringify {
     return $self->{name} . '(' . (join ',', (map $_->stringify, $self->{params})) . ')';
 }
 
+##############################################################################
 package CATS::Formal::Expressions::Access;
 use parent -norequire, 'CATS::Formal::Expressions::BaseExpression';
 sub is_access{1;}
 
+##############################################################################
 package CATS::Formal::Expressions::MemberAccess;
 #head - expr, member - fd
 use parent -norequire , 'CATS::Formal::Expressions::Access';
@@ -72,6 +77,7 @@ sub stringify {
     $_[0]->{head}->stringify . '.' . $_[0]->{member}->{name};
 }
 
+##############################################################################
 package CATS::Formal::Expressions::ArrayAccess;
 use parent -norequire, 'CATS::Formal::Expressions::Access';
 sub is_array_access{1;}
@@ -79,6 +85,7 @@ sub stringify {
     $_[0]->{head}->stringify . '[' . $_[0]->{index}->stringify . ']';
 }
 
+##############################################################################
 package CATS::Formal::Expressions::Constant;
 #$$self - scalar
 use parent -norequire, 'CATS::Formal::Expressions::BaseExpression';
@@ -95,23 +102,22 @@ sub stringify {
     $$self;
 }
 
+##############################################################################
 package CATS::Formal::Expressions::String;
-
 use parent -norequire , 'CATS::Formal::Expressions::Constant';
 sub is_string{1;}
 
-
+##############################################################################
 package CATS::Formal::Expressions::Integer;
-
 use parent -norequire , 'CATS::Formal::Expressions::Constant';
 sub is_int{1;}
 
-
+##############################################################################
 package CATS::Formal::Expressions::Float;
-
 use parent -norequire , 'CATS::Formal::Expressions::Constant';
 sub is_float{1;}
 
+##############################################################################
 package CATS::Formal::Expressions::Array;
 #@$self
 use parent -norequire , 'CATS::Formal::Expressions::Constant';
