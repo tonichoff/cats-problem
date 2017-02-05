2dbdd7bbca2bea4db957ce48bb8301ad4d134414
Alexander Klenin <klenin@gmail.com>
2017-02-05 21:08:56 +1000

    ParserMockup: Store notes and warnings

--- t/ParserMockup.pm
+++ t/ParserMockup.pm
@@ -1,9 +1,9 @@
 package Logger;
 
 #use Carp;
-sub new { bless {}, $_[0]; }
-sub note {}
-sub warning {}
+sub new { bless { notes => [], warnings => [] }, $_[0]; }
+sub note { push @{$_[0]->{notes}}, $_[1] }
+sub warning { push @{$_[0]->{warnings}}, $_[1] }
 sub error { die $_[1] }
 
 package ParserMockup;
