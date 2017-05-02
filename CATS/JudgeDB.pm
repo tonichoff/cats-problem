ba719571ad96142b2393d78f6776f3ad47a1c211
Lukaviy <jiukaviy@gmail.com>
2017-05-02 15:42:53 +1000

    JudgeDB::get_problem: Select 'save_output_prefix'

--- CATS/JudgeDB.pm
+++ CATS/JudgeDB.pm
@@ -28,7 +28,7 @@ sub get_problem {
         SELECT
             id, title, upload_date, $limits_str,
             input_file, output_file, std_checker, contest_id, formal_input,
-            run_method, players_count
+            run_method, players_count, save_output_prefix
         FROM problems WHERE id = ?~, { Slice => {}, ib_timestampformat => '%d-%m-%Y %H:%M:%S' }, $pid);
     $problem->{run_method} //= $cats::rm_default;
     $problem;
