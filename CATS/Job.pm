d3e8ac45dc2c2aec32f4b6e1750b1424a2b8789f
aleksandra zhikhareva <unvergaenglichkeit@gmail.com>
2018-05-29 15:41:40 +1000

    Add problem initialization job type

--- CATS/Job.pm
+++ CATS/Job.pm
@@ -10,21 +10,24 @@ sub create {
     my ($type, $fields) = @_;
 
     $fields ||= {};
-
+    $fields->{state} ||= $cats::job_st_waiting;
     my $rid = new_id;
 
+    $fields->{start_time} = \'CURRENT_TIMESTAMP' if $fields->{state} == $cats::job_st_in_progress;
     $dbh->do(_u $sql->insert('jobs', {
         %$fields,
         id => $rid,
         type => $type,
-        state => $cats::job_st_waiting,
         create_time => \'CURRENT_TIMESTAMP',
     })) or return;
 
-    $dbh->do(q~
-        INSERT INTO jobs_queue (id) VALUES (?)~, undef,
-        $rid) or return;
+    if ($fields->{state} == $cats::job_st_waiting) {
+        $dbh->do(q~
+            INSERT INTO jobs_queue (id) VALUES (?)~, undef,
+            $rid) or return;
+    }
 
+    $dbh->commit;
     $rid;
 }
 
