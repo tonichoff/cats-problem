57d40355606e4d9f034f63046ab51c153852ef6a
FeltBoots <donskoi.ia@students.dvfu.ru>
2019-09-24 13:26:05 +1000

    Repository: Add git clean

--- CATS/Problem/Repository.pm
+++ CATS/Problem/Repository.pm
@@ -997,6 +997,12 @@ sub reset {
     return $self;
 }
 
+sub clean {
+    my ($self, $arg) = @_;
+    $self->git("clean $arg");
+    return $self;
+}
+
 sub checkout {
     my ($self, $what) = @_;
     $what //= '.';
